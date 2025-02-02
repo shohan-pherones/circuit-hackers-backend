19ac8e4dd8a2d9de2e7c6a40d37c612c5f600696
shohan-pherones <shohan.sub.56@gmail.com>
2025-02-03 04:40:18 +0600

    updates cors further

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization, X-Requested-With',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
