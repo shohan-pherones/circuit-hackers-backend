012319842528081f09313236ca88f69621be1d8b
Md. Shohanur Rahman <99276071+shohan-pherones@users.noreply.github.com>
2025-02-03 17:12:51 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true);
 
   app.enableCors({
     origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
